cc8f97e3d381c0cec5d6ab0e534691bea2bbac01
Your Name <you@example.com>
2023-04-04 23:21:47 +0300

    feat: doing slider (Tue, Apr 04, 2023 11:21:47 PM)

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -46,6 +46,29 @@ document.addEventListener('DOMContentLoaded', () => {
 	const width = window.getComputedStyle(sliderWrapper).width;
 
 
+
+
+	async function getPets() {
+		const result = await fetch('../../pets.json');
+		const data = await result.json();
+		console.log('Data',data);
+		let randomArray = [];
+		// while (randomArray.length < 7) {
+
+		for (let i = 0; i < 3; i++) {
+			let randomNumber = Math.floor(Math.random() * 8);
+			if(!randomArray.includes(randomNumber)) {
+				randomArray[i] = randomNumber;
+			} else {
+				i--;
+			}
+		}
+		// }
+		console.log('ARRA',randomArray);
+	}
+	getPets();
+
+
 	// const moveLeft =  () => {
 		// slider.classList.add('transition-left');
 		// prevButton.removeEventListener('click', moveLeft);
@@ -58,24 +81,129 @@ document.addEventListener('DOMContentLoaded', () => {
 		// nextButton.removeEventListener('click', moveRight);
 	// };
 
+
+	// let countLeftClickBtn = 0;
+	// let countRightClickBtn = 0;
+
 	prevButton.addEventListener('click', () => {
 		slider.classList.add('transition-left');
 	});
+
 	nextButton.addEventListener('click', () => {
 		slider.classList.add('transition-right');
 	});
 
 
-	slider.addEventListener('animationend', (animationEvent) => {
-		
-		slider.classList.remove('transition-left');
-		slider.classList.remove('transition-right');
-		prevButton.addEventListener('click', () => {
-			slider.classList.add('transition-left');
-		});
-		nextButton.addEventListener('click', () => {
-			slider.classList.add('transition-right');
-		});
+	const random_start = 1; // От какого генерировать
+	const random_end = 5; // До какого генерировать
+
+	const allСycles = 5;
+
+	let array= [];
+
+	for(let i=random_start;i<=random_end;i++){
+		array.push(i);
+	}
+
+	for(let countCycles=1;countCycles<=allСycles;countCycles++){
+		console.log(array.splice(Math.random()*array.length,1)[0]);
+	}
+
+
+
+	slider.addEventListener('animationend', (animation) => {
+
+		if(animation.animationName === 'move-left') {
+			slider.classList.remove('transition-left');
+			let leftItem = document.querySelector('#item-left').innerHTML;
+			let activeItem = document.querySelector('#item-active').innerHTML;
+
+			document.querySelector('#item-active').innerHTML = leftItem;
+			document.querySelector('#item-right').innerHTML = activeItem;
+
+			///
+			const item1 = document.createElement('div');
+			item1.classList.add('slider__item');
+			item1.innerHTML = `
+				<img src="../../assets/images/our_friends/pets-katrine.png" alt="pet_photo" class="slider__img-pet">
+				<div class="slider__name-pet">${Math.floor(Math.random() * 8)}</div>
+				<button class="button button_hover slider__button">Learn more</button>
+			`;
+
+			const item2 = document.createElement('div');
+			item2.classList.add('slider__item');
+			item2.innerHTML = `
+				<img src="../../assets/images/our_friends/pets-katrine.png" alt="pet_photo" class="slider__img-pet">
+				<div class="slider__name-pet">${Math.floor(Math.random() * 8)}</div>
+				<button class="button button_hover slider__button">Learn more</button>
+			`;
+
+			const item3 = document.createElement('div');
+			item3.classList.add('slider__item');
+			item3.innerHTML = `
+				<img src="../../assets/images/our_friends/pets-katrine.png" alt="pet_photo" class="slider__img-pet">
+				<div class="slider__name-pet">${Math.floor(Math.random() * 8)}</div>
+				<button class="button button_hover slider__button">Learn more</button>
+			`;
+
+			document.querySelector('#item-left').innerHTML = '';
+			document.querySelector('#item-left').appendChild(item1);
+			document.querySelector('#item-left').appendChild(item2);
+			document.querySelector('#item-left').appendChild(item3);
+
+		}
+
+
+		if(animation.animationName === 'move-right') {
+			slider.classList.remove('transition-right');
+
+			let rightItem = document.querySelector('#item-right').innerHTML;
+			let activeItem = document.querySelector('#item-active').innerHTML;
+
+			document.querySelector('#item-active').innerHTML = rightItem;
+			document.querySelector('#item-left').innerHTML = activeItem;
+
+
+
+			///
+			const item1 = document.createElement('div');
+			item1.classList.add('slider__item');
+			item1.innerHTML = `
+				<img src="../../assets/images/our_friends/pets-katrine.png" alt="pet_photo" class="slider__img-pet">
+				<div class="slider__name-pet">${Math.floor(Math.random() * 8)}</div>
+				<button class="button button_hover slider__button">Learn more</button>
+			`;
+
+			const item2 = document.createElement('div');
+			item2.classList.add('slider__item');
+			item2.innerHTML = `
+				<img src="../../assets/images/our_friends/pets-katrine.png" alt="pet_photo" class="slider__img-pet">
+				<div class="slider__name-pet">${Math.floor(Math.random() * 8)}</div>
+				<button class="button button_hover slider__button">Learn more</button>
+			`;
+
+			const item3 = document.createElement('div');
+			item3.classList.add('slider__item');
+			item3.innerHTML = `
+				<img src="../../assets/images/our_friends/pets-katrine.png" alt="pet_photo" class="slider__img-pet">
+				<div class="slider__name-pet">${Math.floor(Math.random() * 8)}</div>
+				<button class="button button_hover slider__button">Learn more</button>
+			`;
+
+			document.querySelector('#item-right').innerHTML = '';
+			document.querySelector('#item-right').appendChild(item1);
+			document.querySelector('#item-right').appendChild(item2);
+			document.querySelector('#item-right').appendChild(item3);
+
+		}
+
+
+		// prevButton.addEventListener('click', () => {
+		// 	slider.classList.add('transition-left');
+		// });
+		// nextButton.addEventListener('click', () => {
+		// 	slider.classList.add('transition-right');
+		// });
 		// prevButton.addEventListener('click', moveLeft);
 		// nextButton.addEventListener('click', moveRight);
 	});
@@ -98,7 +226,7 @@ document.addEventListener('DOMContentLoaded', () => {
 	// 	console.log(pets);
 
 	// 	// showPets(data)
-		
+
 	// }
 
 	// getPets().then(() => {
@@ -106,7 +234,7 @@ document.addEventListener('DOMContentLoaded', () => {
 	// 	allSlides.style.width = 270 * slider.length + 'px';
 	// });
 
-	// let offset = 0; 
+	// let offset = 0;
 
 	// nextButton.addEventListener('click', () => {
 	// 	const slider = document.querySelectorAll('.slider__item');
@@ -120,7 +248,7 @@ document.addEventListener('DOMContentLoaded', () => {
 	// 	allSlides.style.transform = `translateX(-${offset}px)`;
 	// });
 
-	
+
 
 
 	//Render pets on html page
